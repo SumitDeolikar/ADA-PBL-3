0ffcadb856c0c6ddc48206d9ad38ada72d69635d
Shauryakamal <148006944+Shauryakamal@users.noreply.github.com>
2023-10-24 14:58:46 +0530

    Update adapbl1.cpp

--- adapbl1.cpp
+++ adapbl1.cpp
@@ -7,6 +7,27 @@ using namespace std;
  
 #define EMPTY_STRING ""
  
+void decode(Node* root, int &index, string str)
+{
+    if (root == nullptr) {
+        return;
+    }
+ 
+    if (isLeaf(root))
+    {
+        cout << root->ch;
+        return;
+    }
+ 
+    index++;
+ 
+    if (str[index] == '0') {
+        decode(root->left, index, str);
+    }
+    else {
+        decode(root->right, index, str);
+    }
+}
 
 void build(string text)
 {
