fc56f1fa30cc13f63bfb0a79723b77d010414614
Raj Aryan <126001732+RajAryan26@users.noreply.github.com>
2023-10-24 15:06:11 +0530

    Update adapbl1.cpp

--- adapbl1.cpp
+++ adapbl1.cpp
@@ -6,7 +6,22 @@
 using namespace std;
  
 #define EMPTY_STRING ""
+
+
+void encode(Node* root, string str, unordered_map<char, string> &huffmanCode)
+{
+    if (root == nullptr) {
+        return;
+    }
  
+    if (isLeaf(root)) {
+        huffmanCode[root->ch] = (str != EMPTY_STRING) ? str : "1";
+    }
+ 
+    encode(root->left, str + "0", huffmanCode);
+    encode(root->right, str + "1", huffmanCode);
+}
+
 void decode(Node* root, int &index, string str)
 {
     if (root == nullptr) {
